e254394ec16541651c26de0fcb63a74686c8fc2e
brauliobriones <braulio.briones@gmail.com>
2017-11-06 16:16:59 -0300

    Fix Agreements

--- src/pages/agreements/agreements.ts
+++ src/pages/agreements/agreements.ts
@@ -14,6 +14,7 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
   templateUrl: 'agreements.html',
 })
 export class AgreementsPage {
+  pet: string = "puppies";
 
   constructor(public navCtrl: NavController, public navParams: NavParams) {
   }
