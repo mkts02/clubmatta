3298614bf102206b85e34ec863d4095f953e17b8
felipeorellanacabrera <foc.alumno@gmail.com>
2017-11-07 15:20:09 -0300

    universidadesLista

--- src/pages/agreements/agreements.ts
+++ src/pages/agreements/agreements.ts
@@ -14,7 +14,7 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
   templateUrl: 'agreements.html',
 })
 export class AgreementsPage {
-  pet: string = "puppies";
+  convenios: string = "universidades";
 
   constructor(public navCtrl: NavController, public navParams: NavParams) {
   }
