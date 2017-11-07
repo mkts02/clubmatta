42ffd61993c875e561ab79361a50a34d8e71e73a
brauliobriones <braulio.briones@gmail.com>
2017-11-07 18:13:38 -0300

    AgreementsPage - BD

--- src/pages/agreements/agreements.ts
+++ src/pages/agreements/agreements.ts
@@ -1,5 +1,13 @@
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import 
+{ 
+    IonicPage, 
+    NavController, 
+    NavParams 
+} 
+from 'ionic-angular';
+import { AuthServiceProvider } from "../../providers/auth-service";
+import { Common } from '../../providers/common';
 
 /**
  * Generated class for the AgreementsPage page.
@@ -16,11 +24,82 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 export class AgreementsPage {
   convenios: string = "universidades";
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  public resposeDataUniversities: any;
+  public dataSetUniversities: any;
+  public resposeDataInstitutes: any;
+  public dataSetInstitutes: any;
+
+  userPostDataUniversities = {
+    "agreemet_id": "universidad"
+  };
+  userPostDataInstitutes = {
+    "agreemet_id": "instituto"
+  };
+
+  constructor(
+    public common: Common,
+    public navCtrl: NavController, 
+    public navParams: NavParams,
+    public authService: AuthServiceProvider) {
+
+      this.getUniversities();
+
   }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad AgreementsPage');
   }
 
-}
+  getUniversities() {
+    
+    let com = this.common;
+    com.presentLoading();
+
+    this.authService
+      .postData(this.userPostDataUniversities, "showAgreements")
+      .then((result) => {
+
+        com.closeLoading();
+
+        this.resposeDataUniversities = result;
+        if (this.resposeDataUniversities.agreements) {
+          this.dataSetUniversities = this.resposeDataUniversities.agreements;
+          //console.log(this.dataSet);
+
+        } else {
+          console.log("No access");
+        }
+
+      }, (err) => {
+        com.closeLoading();
+        //Connection failed message
+      });
+  }// cierra metodo getUniversities()
+
+  getInstitutes() {
+    
+    let com = this.common;
+    com.presentLoading();
+
+    this.authService
+      .postData(this.userPostDataInstitutes, "showAgreements")
+      .then((result) => {
+
+        com.closeLoading();
+
+        this.resposeDataInstitutes = result;
+        if (this.resposeDataInstitutes.agreements) {
+          this.dataSetInstitutes = this.resposeDataInstitutes.agreements;
+          //console.log(this.dataSet);
+
+        } else {
+          console.log("No access");
+        }
+
+      }, (err) => {
+        com.closeLoading();
+        //Connection failed message
+      });
+  }// cierra metodo getInstitutes()
+
+}// cierra la clase
