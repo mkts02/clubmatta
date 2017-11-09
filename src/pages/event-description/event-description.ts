325416101221bd64840c922f89f74d67870e6270
cristobal <cristobalherreramkts@live.cl>
2017-11-09 18:10:45 -0300

    modal event description working

--- src/pages/event-description/event-description.ts
+++ src/pages/event-description/event-description.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { IonicPage, NavController, NavParams,  ModalController } from 'ionic-angular';
+import { EventInvitationModalPage } from '../event-invitation-modal/event-invitation-modal';
+import { NativePageTransitions, NativeTransitionOptions } from '@ionic-native/native-page-transitions';
 
 
 @IonicPage()
@@ -13,12 +15,33 @@ export class EventDescriptionPage {
 
   item;
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  constructor(public navCtrl: NavController, 
+    public navParams: NavParams,
+    private nativePageTransitions: NativePageTransitions,
+    public modalCtrl: ModalController,) {
     this.item = navParams.data.item;
   }
 
   ionViewDidLoad() {
     //console.log('ionViewDidLoad EventdescriptionPage');
   }
+
+  showInvitationModal(item) {
+    
+    let options: NativeTransitionOptions = {
+      direction: 'right',
+      duration: 800,
+      slowdownfactor: 3,
+      slidePixels: 20,
+      iosdelay: 100,
+      androiddelay: 150,
+      fixedPixelsTop: 0,
+      fixedPixelsBottom: 60
+     };
+      this.nativePageTransitions.fade(options);
+      const profileModal = this.modalCtrl.create(EventInvitationModalPage, { item: item });
+      profileModal.present();
+    
+    }
 
 }
